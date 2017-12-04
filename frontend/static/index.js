b1409d58dadc46cd77afbd3afee9b6c1631d46de
Jacques Labuschagne <jlabusch@acm.org>
2017-12-04 12:58:56 +0000

    Band-aid on index page API-storm performance issues.

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -8,57 +8,94 @@ function render(name, i, color){
     );
 }
 
-function make_renderer(name, i){
+function handle_hours(name, i, next){
+    return function(cerr, data) {
+        if (cerr){
+            console.log('sla_quotes: ' + JSON.stringify(qerr));
+            next();
+            return;
+        }
+        if (!data.budget){
+            console.log(name + ' has no budget, not applying color');
+            next();
+            return;
+        }
+        if (!data.result || data.result.length < 3){
+            console.log(name + ' has invalid "result" array for client data, not applying color');
+            next();
+            return;
+        }
+        var budget = data.budget,
+            total = data.result[0][1] /* quoted */ +
+                    data.result[1][1] /* unquoted */,
+            color = "red";
+
+        if (total < budget * 0.75) {
+            color = "green";
+        } else if (total < budget) {
+            color = "yellow";
+        }
+
+        next(name, i, color);
+    };
+}
+
+function fetch_hours(name, i, next){
     return function(qerr) {
         if (qerr){
             console.log('sla_quotes: ' + JSON.stringify(qerr));
+            next();
             return;
         }
-        query("/sla_hours",
-            function(cerr, data) {
-                if (cerr){
-                    console.log('sla_quotes: ' + JSON.stringify(qerr));
-                    return;
-                }
-                if (!data.budget){
-                    console.log(name + ' has no budget, not applying color');
-                    return;
-                }
-                if (!data.result || data.result.length < 3){
-                    console.log(name + ' has invalid "result" array for client data, not applying color');
-                    return;
-                }
-                var budget = data.budget,
-                    total = data.result[0][1] /* quoted */ +
-                            data.result[1][1] /* unquoted */,
-                    color = "red";
-
-                if (total < budget * 0.75) {
-                    color = "green";
-                } else if (total < budget) {
-                    color = "yellow";
-                }
-
-                render(name, i, color);
-            },
+        query(
+            "/sla_hours",
+            handle_hours(name, i, next),
             undefined,
             undefined,
-            name + "/default/" + PERIOD);
+            name + "/default/" + PERIOD
+        );
     }
 }
 
-function fetch_data(name, i) {
+function fetch_quotes_from_queue(head, rest){
+    if (!head){
+        return;
+    }
+    function after_fetches(name, i, color){
+        if (name){
+            render(name, i, color);
+        }
+        fetch_quotes_from_queue(rest.shift(), rest);
+    }
+    query(
+        '/sla_quotes',
+        fetch_hours(head.name, head.index, after_fetches),
+        undefined,
+        undefined,
+        head.uri
+    );
+}
+
+var query_list = [];
+
+function build_queue(name, i) {
     if (FIRST_LOAD) {
         render(name, i, 'blue');
     }
-    query("/sla_quotes", make_renderer(name, i), undefined, undefined, name + "/default/" + PERIOD);
+    query_list.push({
+        name: name,
+        index: i,
+        uri: name + '/default/' + PERIOD
+    });
 }
 
 query('/customer_list', function(err, data){
     if (err){
-        console.log(err);
+        console.log('customer_list: ' + err);
         return;
     }
-    Object.keys(data).sort().forEach(fetch_data);
+    query_list = [];
+    Object.keys(data).sort().forEach(build_queue);
     FIRST_LOAD = false;
+    fetch_quotes_from_queue(query_list.shift(), query_list);
 });
