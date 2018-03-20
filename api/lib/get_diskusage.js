ab8cff83c89bb538365ebdf582a920e22d2f92f1
Botond Hegedus <22196488+botondhegedus@users.noreply.github.com>
2018-03-20 12:23:38 +0000

    Create get_diskusage.js

---
+++ api/lib/get_diskusage.js
@@ -0,0 +1,73 @@
+var query = require('./query'),
+    https  = require('https'),
+    cache = require('./cache'),
+    util  = require('./util');
+
+module.exports = query.prepare(
+    'users',
+    'users',
+    null,
+    function(data, ctx, next){
+        let r = {
+            result: data
+        };
+        next(r);
+    },
+    (key, ctx, next, error) => {
+        if (!util.get_org(ctx) ||
+            !util.get_org(ctx).users ||
+            !util.get_org(ctx).users.hostname ||
+            !util.get_org(ctx).users.token)
+        {
+            return error('No user lookup configured for ' + ctx.org);
+        }
+
+        let u = util.get_org(ctx).users;
+
+        const template = '/webservice/rest/server.php?wsfunction=tool_diskusage_api&' +
+                'moodlewsrestformat=json&' +
+                'wstoken=USER_TOKEN&' +
+                'duration=1&' +
+                'duration_unit=year';
+
+        const options = {
+            path: template.replace('USER_TOKEN', u.token),
+            protocol: 'https:',
+            hostname: u.hostname
+        }
+
+        let req = https.request(options, (res) => {
+            // This will only have a non-200 OK in pretty exceptional circumstances.
+            if (res.statusCode !== 200) {
+                let e = 'users: ' + options.hostname + ' => ' + res.statusCode;
+                util.log(__filename, e);
+                return error(e);
+            }
+
+            let data = '';
+            res.on('data', chunk => { data += chunk });
+            res.on('end', () => {
+                let json = {};
+                try {
+                    json = JSON.parse(data);
+                    if (json.exception) {
+                        // Something went wrong with the WS request itself, eg bad token.
+                        let msg = json.message ? json.message : json.exception;
+                        util.log(__filename, msg);
+                        return error(msg);
+                    }
+                    cache.put(key, json);
+                    util.log_debug(__filename, 'users: ' + json.count);
+                } catch (ex) {
+                    let e = 'users: ' + ex;
+                    util.log(__filename, e);
+                    return error(e);
+                }
+                next({result: json.count});
+            });
+        });
+        req.on('error', error);
+        req.end();
+    },
+    60*60*1000 // cache TTL 1 hour
+);
