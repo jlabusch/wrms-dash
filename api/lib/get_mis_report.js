42f5b76aa66669a973dae6b24f8c22e0f20c5f94
Jacques Labuschagne <jlabusch@acm.org>
2018-05-22 21:46:21 +0100

    Fix MIS report column headings.

--- api/lib/get_mis_report.js
+++ api/lib/get_mis_report.js
@@ -11,9 +11,13 @@ module.exports = function(req, res, next, ctx){
         let raw = fs.readFileSync('./mis.json', {encoding: 'utf8'}),
             json = JSON.parse(raw);
 
+        let months = ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar'];
+        if (json.result.header.length > 0){
+            months = json.result.header[0].cols.map(col => { return col.date.replace(' (EU)', '') }).slice(0, 12).reverse();
+        }
+
         json.result.content.forEach(obj => {
             if (obj.kpi_name === 'Sales Revenue'){
-                let months = ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar'];
                 resp.result = obj.cols.slice(0, 12).reverse().map(col => { return [months.shift(), col.val]; });
             }
         });
