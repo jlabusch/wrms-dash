b8cec96844053cf8ac4f3420302bad0c0756ff1f
Jacques Labuschagne <jlabusch@acm.org>
2018-07-18 21:26:23 +0100

    Splitting sync into main/utils

--- api/lib/data_sync.js
+++ api/lib/data_sync.js
@@ -1,6 +1,6 @@
 var config  = require('config'),
-    org_data= require('./org_data'),
     util    = require('./util'),
+    su      = require('./data_sync_utils'),
     fs      = require('fs'),
     store   = require('./data_store'),
     espo    = require('./espo'),
@@ -15,30 +15,6 @@ let sync_active = true,
     sql = store.sql;
 
 const DEBUG = false;
-
-// select_best_budget() relies on this key composition.
-function create_budget_name(contract, type, period){
-    return contract.name + ' ' + type + ' ' + period;
-}
-
-function match_non_monthly_budget_name(id, ctx){
-    // Matches both annaul and biannual
-    let m = id.match(/annual (\d\d\d\d-\d?\d) to (\d\d\d\d-\d?\d)/);
-
-    if (m){
-        let from = new Date(m[1]),
-            to = new Date(m[2]);
-
-        // force "to" to be the end of the month
-        to.setMonth(to.getMonth()+1);
-        to.setDate(-1);
-
-        let pdate = new Date(ctx.period);
-
-        return pdate > from && pdate < to;
-    }
-    return false;
-}
 
 async function run(){
     util.log_debug(__filename, 'run()', DEBUG);
@@ -56,7 +32,7 @@ async function run(){
             await sync();
             // store.dbs.syncing is now fresh, make it active.
             store.swap();
-            dump(store.dbs.active, 'After swap');
+            su.dump(store.dbs.active, 'After swap');
             util.log_debug(__filename, 'sync complete', DEBUG);
         }catch(err){
             util.log(__filename, 'sync error, keeping current DB active');
@@ -89,41 +65,13 @@ function sync(){
     });
 }
 
-function dump_rows_as_csv(rows){
-    if (Array.isArray(rows) && rows.length > 0){
-        console.log(Object.keys(rows[0]).join(','));
-        rows.forEach(row => {
-            console.log(Object.values(row).join(','));
-        });
-    }
-}
-
-function dump(db, label = ''){
-    if (!DEBUG){
-        return;
-    }
-    Object.keys(sql.dump).forEach(key => {
-        db.all(
-            sql.dump[key],
-            (err, res) => {
-                if (err){
-                    util.log(__filename, `ERROR in dump()/${key}: ${err.message || err}`);
-                    return;
-                }
-                console.log('======================== ' + label + ' ========================');
-                dump_rows_as_csv(res);
-            }
-        );
-    });
-}
-
 function sync_contract(c){
     util.log_debug(__filename, `sync_contract(${c.name})`, DEBUG);
 
     return new Promise((resolve, reject) => {
         create_contract(store.dbs.syncing, c).then(
             fetch_wrs_from_wrms(resolve, reject, c),
-            soft_failure(resolve, `ERROR creating contract ${c.name}: `)
+            su.soft_failure(resolve, `ERROR creating contract ${c.name}: `)
         );
     });
 }
@@ -157,7 +105,7 @@ function fetch_wrs_from_wrms(resolve, reject, contract){
             null // no context for this call
         ).then(
             format_timesheet_dates(resolve, reject, contract),
-            soft_failure(resolve, `ERROR getting WRs for systems ${JSON.stringify(contract.systems)}: `)
+            su.soft_failure(resolve, `ERROR getting WRs for systems ${JSON.stringify(contract.systems)}: `)
         );
     }
 }
@@ -184,20 +132,8 @@ function fetch_quotes_from_wrms(resolve, reject, contract, wr_rows){
         null // no context for this call
     ).then(
         process_wrms_data(resolve, reject, contract, wr_rows),
-        soft_failure(resolve, `ERROR getting quotes for systems ${JSON.stringify(contract.systems)}: `)
+        su.soft_failure(resolve, `ERROR getting quotes for systems ${JSON.stringify(contract.systems)}: `)
     );
-}
-
-function set_wr_tags_from_string(wr, str){
-    wr.additional = false;
-    wr.unchargeable = false;
-
-    if (str){
-        wr.additional = str.includes('Additional');
-        wr.unchargeable = str.includes('Warranty') || str.includes('Maintenance');
-    }
-
-    return wr;
 }
 
 function process_wrms_data(resolve, reject, contract, wr_rows){
@@ -216,7 +152,7 @@ function process_wrms_data(resolve, reject, contract, wr_rows){
 
                 util.log_debug(__filename, `contract ${contract.name} WR ${wr.request_id}: ${wr.brief} [${wr.status}] ${wr.tags || ''}`, DEBUG);
 
-                set_wr_tags_from_string(wr, wr.tags);
+                su.set_wr_tags_from_string(wr, wr.tags);
 
                 try{
                     await sqlite_promise(
@@ -238,13 +174,13 @@ function process_wrms_data(resolve, reject, contract, wr_rows){
                     );
                 }catch(err){
                     util.log(__filename, `ERROR adding synced WR ${wr.request_id}: ${err}`);
-                    iw = find_last_row_for_this_wr(wr_rows, iw);
+                    iw = su.find_last_row_for_this_wr(wr_rows, iw);
                     continue;
                 }
 
                 if (wr.unchargeable){
                     util.log_debug(__filename, `WR ${wr.request_id} not chargeable, skipping quotes and timesheets`);
-                    iw = find_last_row_for_this_wr(wr_rows, iw);
+                    iw = su.find_last_row_for_this_wr(wr_rows, iw);
                     continue;
                 }
 
@@ -298,7 +234,7 @@ function process_wrms_data(resolve, reject, contract, wr_rows){
                     // If the quote is approved, modify the budget in light of this quote
                     if (q.approved_by_id){
                         try{
-                            await modify_budget_for_quote(budget, q, qdesc);
+                            await su.modify_budget_for_quote(budget, q, qdesc);
                         }catch(err){
                             util.log(__filename, `ERROR modifying budget for ${contract.name} period ${qdesc.period}: ${err}`);
                             await sqlite_promise(store.dbs.syncing, 'ROLLBACK').catch(err => {});
@@ -307,7 +243,7 @@ function process_wrms_data(resolve, reject, contract, wr_rows){
                     }
 
                     try{
-                        await add_quote(q, qdesc, budget, wr);
+                        await su.add_quote(q, qdesc, budget, wr);
                     }catch(err){
                         util.log(__filename, `ERROR adding synced quote ${q.quote_id}: ${err}`);
                         await sqlite_promise(store.dbs.syncing, 'ROLLBACK');
@@ -317,7 +253,7 @@ function process_wrms_data(resolve, reject, contract, wr_rows){
                         util.log(__filename, `ERROR committing quote ${q.quote_id}: ${err}`);
                     });
 
-                    if (quote_is_valid(q)){
+                    if (su.quote_is_valid(q)){
                         has_quotes = true;
                     }
                 }
@@ -326,7 +262,7 @@ function process_wrms_data(resolve, reject, contract, wr_rows){
 
                 if (has_quotes){
                     util.log_debug(__filename, `WR ${wr.request_id} has quotes, so skipping timesheets`);
-                    iw = find_last_row_for_this_wr(wr_rows, iw);
+                    iw = su.find_last_row_for_this_wr(wr_rows, iw);
                     continue;
                 }
             }
@@ -426,14 +362,6 @@ function process_wrms_data(resolve, reject, contract, wr_rows){
     }
 }
 
-// Soft failures shouldn't stop the other contracts from being created.
-function soft_failure(resolve , msg){
-    return function(err){
-        util.log(__filename, msg + (err.message || err));
-        resolve(false);
-    }
-}
-
 // Loads contracts from the static config (config.contracts) and then optionally also
 // from Espo CRM (config.espo.enabled) file (config.contracts). It but WILL pick up runtime
 // contract changes, it doesn't use require('config').
@@ -488,59 +416,6 @@ function load_static_contracts(){
     });
 }
 
-function remove_previous_contract_budgets(db, contract){
-    let stmts = [
-        sql.delete_contract_budgets,
-        sql.delete_contract_budget_links
-    ];
-    return util.promise_sequence(stmts, s => {
-        return sqlite_promise(db, s, contract.name);
-    });
-}
-
-function add_new_contract_and_systems(db, contract){
-    let stmt_fns = [];
-
-    org_data.add_org(contract);
-    stmt_fns.push(store.generate_sqlite_promise(db, sql.add_contract, contract.name, contract.org_id, contract.org_name, contract.start_date, contract.end_date));
-
-    for (let i = 0; i < contract.systems.length; ++i){
-        org_data.add_system(contract, contract.systems[i]);
-        stmt_fns.push(store.generate_sqlite_promise(db, sql.add_system, contract.systems[i]));
-        stmt_fns.push(store.generate_sqlite_promise(db, sql.add_contract_system_link, contract.name, contract.systems[i]));
-    }
-
-    return util.promise_sequence(stmt_fns, fn => {
-        return fn();
-    });
-}
-
-function add_budgets_for_contract(db, contract){
-    let current = new Date(contract.start_date),
-        end = new Date(contract.end_date),
-        stmt_fns = [];
-
-    // For more reliable date incrementing:
-    current.setDate(1);
-    current.setHours(8);
-    // And disambiguate contracts that end on the last day of the month
-    end.setDate(end.getDate()+1);
-
-    while (current < end){
-        let [err, key] = make_budget_name_and_increment_date(contract, current, end);
-        if (err){
-            return err;
-        }else{
-            stmt_fns.push(store.generate_sqlite_promise(db, sql.add_budget, key, contract.hours));
-            stmt_fns.push(store.generate_sqlite_promise(db, sql.add_contract_budget_link, contract.name, key));
-        }
-    }
-
-    return util.promise_sequence(stmt_fns, fn => {
-        return fn();
-    });
-}
-
 // Create the inferred/non-quote budgets.
 function create_contract(db, c){
     return new Promise((resolve, reject) => {
@@ -553,9 +428,9 @@ function create_contract(db, c){
 
         util.promise_sequence(
             [
-                remove_previous_contract_budgets,
-                add_new_contract_and_systems,
-                add_budgets_for_contract
+                su.remove_previous_contract_budgets,
+                su.add_new_contract_and_systems,
+                su.add_budgets_for_contract
             ],
             fn => {
                 return fn(db, c);
@@ -564,38 +439,6 @@ function create_contract(db, c){
         .then(resolve)
         .catch(reject);
     });
-}
-
-function make_budget_name_and_increment_date(contract, current, end){
-    let this_month = util.date_fmt(current),
-        key = null;
-
-    switch(contract.type){
-        case 'monthly':
-            current.setMonth(current.getMonth()+1);
-            key = create_budget_name(contract, 'month', this_month);
-            break;
-        case '6 monthly':
-            current.setMonth(current.getMonth()+6);
-            key = create_budget_name(contract, 'biannual', this_month + ' to ' + util.date_fmt(current));
-            break;
-        case 'annually':
-            current.setMonth(current.getMonth()+12);
-            key = create_budget_name(contract, 'annual', this_month + ' to ' + util.date_fmt(current));
-            break;
-        default:
-            contract.setTime(end.getTime());
-            return [new Error(`Unsupported contract type "${contract.type}"`)];
-    }
-    return [null, key];
-}
-
-function find_last_row_for_this_wr(arr, i){
-    let id = arr[i].request_id;
-    while (i < arr.length && arr[i].request_id === id){
-        ++i;
-    }
-    return i-1;
 }
 
 // Order of preference:
@@ -615,7 +458,7 @@ function select_best_budget(contract, period){
                     return;
                 }
 
-                let monthly_name = create_budget_name(contract, 'month', period);
+                let monthly_name = su.create_budget_name(contract, 'month', period);
 
                 // First try to find an existing budget...
                 if (Array.isArray(budgets) && budgets.length > 0){
@@ -625,7 +468,7 @@ function select_best_budget(contract, period){
                     budgets.forEach(budget => {
                         if (budget.id == monthly_name){
                             month_match = budget;
-                        }else if (match_non_monthly_budget_name(budget.id, {period:period})){
+                        }else if (su.match_non_monthly_budget_name(budget.id, {period:period})){
                             annual_match = budget;
                         }
                     });
@@ -669,53 +512,11 @@ function select_best_budget(contract, period){
     });
 }
 
-function modify_budget_for_quote(budget, quote, quote_description){
-    if (quote_description.additional){
-        // Additional quote hours are recorded as a separate tally on additional_hours
-        return sqlite_promise(
-            store.dbs.syncing,
-            'UPDATE budgets SET additional_hours=? WHERE id=?',
-            budget.additional_hours + quote.quote_amount,
-            budget.id
-        );
-    }else{
-        // SLA quote hours are subtracted from base_hours as base_hours_spent
-        return sqlite_promise(
-            store.dbs.syncing,
-            'UPDATE budgets SET base_hours_spent=?, sla_quote_hours=? WHERE id=?',
-            budget.base_hours_spent + quote.quote_amount,
-            budget.sla_quote_hours + quote.quote_amount,
-            budget.id
-        );
-    }
-}
-
-function quote_is_valid(quote){
-    return !!quote.approved_by_id || !quote.expired;
-}
-
-function add_quote(quote, desc, budget, wr){
-    util.log_debug(__filename, `add_quote(${JSON.stringify(quote)})`, DEBUG);
-
-    return sqlite_promise(
-        store.dbs.syncing,
-        sql.add_quote,
-        quote.quote_id,
-        budget.id,
-        quote.request_id,
-        util.date_fmt(new Date(desc.period)), // take wrs.invoice_to and quotes.approved_on into account
-        quote.quote_amount,
-        desc.additional|0,
-        quote_is_valid(quote)|0,
-        !!quote.approved_by_id|0
-    );
-}
-
 setTimeout(run, config.get('sync.startup_delay_secs')*1000);
 
 module.exports = {
-    create_budget_name: create_budget_name,
-    match_non_monthly_budget_name: match_non_monthly_budget_name,
+    create_budget_name: su.create_budget_name,
+    match_non_monthly_budget_name: su.match_non_monthly_budget_name,
     pause: () => { sync_active = false },
     unpause: () => { sync_active = true }
 }
---
+++ api/lib/data_sync_utils.js
@@ -0,0 +1,239 @@
+var config  = require('config'),
+    org_data= require('./org_data'),
+    util    = require('./util'),
+    fs      = require('fs'),
+    store   = require('./data_store'),
+    espo    = require('./espo');
+
+'use strict';
+
+let sync_active = true,
+    first_run = true,
+    sqlite_promise = store.sqlite_promise,
+    sql = store.sql;
+
+const DEBUG = false;
+
+// select_best_budget() relies on this key composition.
+function create_budget_name(contract, type, period){
+    return contract.name + ' ' + type + ' ' + period;
+}
+
+exports.create_budget_name = create_budget_name;
+
+function match_non_monthly_budget_name(id, ctx){
+    // Matches both annaul and biannual
+    let m = id.match(/annual (\d\d\d\d-\d?\d) to (\d\d\d\d-\d?\d)/);
+
+    if (m){
+        let from = new Date(m[1]),
+            to = new Date(m[2]);
+
+        // force "to" to be the end of the month
+        to.setMonth(to.getMonth()+1);
+        to.setDate(-1);
+
+        let pdate = new Date(ctx.period);
+
+        return pdate > from && pdate < to;
+    }
+    return false;
+}
+
+exports.match_non_monthly_budget_name = match_non_monthly_budget_name;
+
+function dump_rows_as_csv(rows){
+    if (Array.isArray(rows) && rows.length > 0){
+        console.log(Object.keys(rows[0]).join(','));
+        rows.forEach(row => {
+            console.log(Object.values(row).join(','));
+        });
+    }
+}
+
+function dump(db, label = ''){
+    if (!DEBUG){
+        return;
+    }
+    Object.keys(sql.dump).forEach(key => {
+        db.all(
+            sql.dump[key],
+            (err, res) => {
+                if (err){
+                    util.log(__filename, `ERROR in dump()/${key}: ${err.message || err}`);
+                    return;
+                }
+                console.log('======================== ' + label + ' ========================');
+                dump_rows_as_csv(res);
+            }
+        );
+    });
+}
+
+exports.dump = dump;
+
+function set_wr_tags_from_string(wr, str){
+    wr.additional = false;
+    wr.unchargeable = false;
+
+    if (str){
+        wr.additional = str.includes('Additional');
+        wr.unchargeable = str.includes('Warranty') || str.includes('Maintenance');
+    }
+
+    return wr;
+}
+
+exports.set_wr_tags_from_string = set_wr_tags_from_string;
+
+// Soft failures shouldn't stop the other contracts from being created.
+function soft_failure(resolve , msg){
+    return function(err){
+        util.log(__filename, msg + (err.message || err));
+        resolve(false);
+    }
+}
+
+exports.soft_failure = soft_failure;
+
+function remove_previous_contract_budgets(db, contract){
+    let stmts = [
+        sql.delete_contract_budgets,
+        sql.delete_contract_budget_links
+    ];
+    return util.promise_sequence(stmts, s => {
+        return sqlite_promise(db, s, contract.name);
+    });
+}
+
+exports.remove_previous_contract_budgets = remove_previous_contract_budgets;
+
+function add_new_contract_and_systems(db, contract){
+    let stmt_fns = [];
+
+    org_data.add_org(contract);
+    stmt_fns.push(store.generate_sqlite_promise(db, sql.add_contract, contract.name, contract.org_id, contract.org_name, contract.start_date, contract.end_date));
+
+    for (let i = 0; i < contract.systems.length; ++i){
+        org_data.add_system(contract, contract.systems[i]);
+        stmt_fns.push(store.generate_sqlite_promise(db, sql.add_system, contract.systems[i]));
+        stmt_fns.push(store.generate_sqlite_promise(db, sql.add_contract_system_link, contract.name, contract.systems[i]));
+    }
+
+    return util.promise_sequence(stmt_fns, fn => {
+        return fn();
+    });
+}
+
+exports.add_new_contract_and_systems = add_new_contract_and_systems;
+
+function add_budgets_for_contract(db, contract){
+    let current = new Date(contract.start_date),
+        end = new Date(contract.end_date),
+        stmt_fns = [];
+
+    // For more reliable date incrementing:
+    current.setDate(1);
+    current.setHours(8);
+    // And disambiguate contracts that end on the last day of the month
+    end.setDate(end.getDate()+1);
+
+    while (current < end){
+        let [err, key] = make_budget_name_and_increment_date(contract, current, end);
+        if (err){
+            return err;
+        }else{
+            stmt_fns.push(store.generate_sqlite_promise(db, sql.add_budget, key, contract.hours));
+            stmt_fns.push(store.generate_sqlite_promise(db, sql.add_contract_budget_link, contract.name, key));
+        }
+    }
+
+    return util.promise_sequence(stmt_fns, fn => {
+        return fn();
+    });
+}
+
+exports.add_budgets_for_contract = add_budgets_for_contract;
+
+function make_budget_name_and_increment_date(contract, current, end){
+    let this_month = util.date_fmt(current),
+        key = null;
+
+    switch(contract.type){
+        case 'monthly':
+            current.setMonth(current.getMonth()+1);
+            key = create_budget_name(contract, 'month', this_month);
+            break;
+        case '6 monthly':
+            current.setMonth(current.getMonth()+6);
+            key = create_budget_name(contract, 'biannual', this_month + ' to ' + util.date_fmt(current));
+            break;
+        case 'annually':
+            current.setMonth(current.getMonth()+12);
+            key = create_budget_name(contract, 'annual', this_month + ' to ' + util.date_fmt(current));
+            break;
+        default:
+            contract.setTime(end.getTime());
+            return [new Error(`Unsupported contract type "${contract.type}"`)];
+    }
+    return [null, key];
+}
+
+function find_last_row_for_this_wr(arr, i){
+    let id = arr[i].request_id;
+    while (i < arr.length && arr[i].request_id === id){
+        ++i;
+    }
+    return i-1;
+}
+
+exports.find_last_row_for_this_wr = find_last_row_for_this_wr;
+
+function modify_budget_for_quote(budget, quote, quote_description){
+    if (quote_description.additional){
+        // Additional quote hours are recorded as a separate tally on additional_hours
+        return sqlite_promise(
+            store.dbs.syncing,
+            'UPDATE budgets SET additional_hours=? WHERE id=?',
+            budget.additional_hours + quote.quote_amount,
+            budget.id
+        );
+    }else{
+        // SLA quote hours are subtracted from base_hours as base_hours_spent
+        return sqlite_promise(
+            store.dbs.syncing,
+            'UPDATE budgets SET base_hours_spent=?, sla_quote_hours=? WHERE id=?',
+            budget.base_hours_spent + quote.quote_amount,
+            budget.sla_quote_hours + quote.quote_amount,
+            budget.id
+        );
+    }
+}
+
+exports.modify_budget_for_quote = modify_budget_for_quote;
+
+function quote_is_valid(quote){
+    return !!quote.approved_by_id || !quote.expired;
+}
+
+exports.quote_is_valid = quote_is_valid;
+
+function add_quote(quote, desc, budget, wr){
+    util.log_debug(__filename, `add_quote(${JSON.stringify(quote)})`, DEBUG);
+
+    return sqlite_promise(
+        store.dbs.syncing,
+        sql.add_quote,
+        quote.quote_id,
+        budget.id,
+        quote.request_id,
+        util.date_fmt(new Date(desc.period)), // take wrs.invoice_to and quotes.approved_on into account
+        quote.quote_amount,
+        desc.additional|0,
+        quote_is_valid(quote)|0,
+        !!quote.approved_by_id|0
+    );
+}
+
+exports.add_quote = add_quote;
+
